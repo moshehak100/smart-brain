d6715d8474572d5009a0bd466cbba72aab242e98
Moshe <moshehak10@gmail.com>
2021-04-15 02:38:06 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
+import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+//registerServiceWorker();
+reportWebVitals();
